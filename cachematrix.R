d22c20d53b7bc41f222ba86fa1514e1bbe76a8b9
Harshal Singh Chauhan <82678836+Harshalhsc007@users.noreply.github.com>
2023-07-09 14:41:48 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# Function to create a special "matrix" object that can cache its inverse
+makeCacheMatrix <- function(x) {
+  # Initialize the cache
+  cache <- NULL
+  
+  # Function to set the matrix
+  set <- function(y) {
+    x <<- y  # Use <<- to assign the value to the parent environment
+    cache <<- NULL  # Reset the cache when the matrix is changed
+  }
+  
+  # Function to get the matrix
+  get <- function() {
+    x
+  }
+  
+  # Function to compute and cache the inverse of the matrix
+  cacheInverse <- function() {
+    if (!is.null(cache)) {
+      # Return the cached inverse if available
+      message("Getting cached inverse")
+      return(cache)
+    } else {
+      # Compute the inverse of the matrix
+      message("Calculating inverse")
+      inv <- solve(x)
+      
+      # Cache the inverse
+      cache <<- inv
+      
+      # Return the inverse
+      inv
+    }
+  }
+  
+  # Return a list of functions
+  list(set = set, get = get, cacheInverse = cacheInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# Function to compute the inverse of the matrix (with caching)
+cacheSolve <- function(x) {
+  # Call the cacheInverse function from the makeCacheMatrix object
+  x$cacheInverse()
 }
+
+# Example usage
+# Create a matrix
+A <- matrix(c(1, 2, 3, 4), nrow = 2)
+
+# Create a special matrix object with caching
+cacheMatrix <- makeCacheMatrix(A)
+
+# Compute the inverse (will calculate and cache it)
+cacheSolve(cacheMatrix)
+
+# Compute the inverse again (will retrieve it from cache)
+cacheSolve(cacheMatrix)
